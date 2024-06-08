797b33473c7b153477daebc39b57684e320155c0
thomasrichmond <thomas.richmond8@gmail.com>
2024-06-09 00:06:57 +1000

    Adding header to root layout

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Server Side Sundays</h1>
+      <img alt="tom-image" src="https://github.com/thomasrichmond.png" />
+    </header>
+  );
+}
+
+export default Header;
