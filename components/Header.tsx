864b28717db19927bdbf65bf4e7ca14c4b77dbe8
thomasrichmond <thomas.richmond8@gmail.com>
2024-06-09 00:12:04 +1000

    Changing icon to use Next/Image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 function Header() {
   return (
     <header>
       <h1>Server Side Sundays</h1>
-      <img alt="tom-image" src="https://github.com/thomasrichmond.png" />
+      <Image
+        alt="tom-image"
+        src="https://github.com/thomasrichmond.png"
+        width={32}
+        height={32}
+      />
     </header>
   );
 }
